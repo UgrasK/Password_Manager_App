8cc0012112ac0ab49d3644b4578a994d79b9939f
UgrasK <ugurkaraer1989@gmail.com>
2022-07-26 12:33:04 +0300

    created window & canvas. centered an image in canvas.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# create window
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+# create canvas & show an image in it
+canvas = Canvas(width=200, height=200)
+logo_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_image)
+canvas.pack()
+
+window.mainloop()
