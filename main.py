4d62fe32646cf4162beb420325e42a6185175d24
ugrasK <ugurkaraer1989@gmail.com>
2022-07-28 18:09:33 +0300

    added find_password function to check & show user input in json with search button.

--- main.py
+++ main.py
@@ -67,6 +67,22 @@ def save():
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} \nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # create window
@@ -89,8 +105,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # create entries
-website_entry = Entry(width=54)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=54)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -99,6 +115,8 @@ password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1)
 
 # create buttons
+search_button = Button(text="Search", width=15, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=46, command=save)
